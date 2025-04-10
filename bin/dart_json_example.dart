204aca33250fca70cd0e710af430e04b08376401
Aislyn-cell <lerrynjeong@gmail.com>
2025-04-10 13:39:05 +0900

    json 직렬화와 역직렬화 실습 코드입니다.

--- bin/dart_json_example.dart
+++ bin/dart_json_example.dart
@@ -1,5 +1,24 @@
-import 'package:dart_json_example/dart_json_example.dart' as dart_json_example;
+import 'dart:convert'; // dart convert 라이브러리 가져오기
 
-void main(List<String> arguments) {
-  print('Hello world: ${dart_json_example.calculate()}!');
+void main() {
+  // Map → JSON 문자열 (직렬화)
+  Map<String, dynamic> myInfo = {"name": "정하나", "age": 21};
+  String jsonString = jsonEncode(myInfo);
+  print('직렬화된 JSON 문자열: $jsonString');
+  // 출력: {"name":"정하나","age":21}
+
+  List list = [myInfo, myInfo];
+  String listJsonString = jsonEncode(list);
+  print('리스트 직렬화된 문자열: $listJsonString');
+  // 출력: [{"name":"정하나","age":21},{"name":"정하나","age":21}]
+
+  // JSON 문자열 → Map (역직렬화)
+  Map<String, dynamic> decodedMap = jsonDecode(jsonString);
+  print('역직렬화된 Map: $decodedMap');
+  // 출력: {name: 정하나, age: 21}
+
+  // 리스트 형태 JSON 문자열 → List<Map>
+  List<dynamic> decodedList = jsonDecode(listJsonString);
+  print('역직렬화된 List: $decodedList');
+  // 출력: [{name: 정하나, age: 21}, {name: 정하나, age: 21}]
 }
